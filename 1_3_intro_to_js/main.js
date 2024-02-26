fb587a733a62671136209972a27242d143ad3859
MickeyD5 <micaiah_davis@yahoo.com>
2024-02-26 15:40:33 -0500

    button click count

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,13 @@
 console.log('hello world');
+
+let myBtn = document.getElementById('button')
+let myClicks = document.getElementById('clicks')
+let clickCount = 0;
+
+myClicks.innerText= `The button was clicked ${clickCount} times!`;
+button.onclick= function updateCount() {
+    clickCount++; 
+    myClicks.innerText = `The button was clicked ${clickCount} times!`;
+}
+
+
